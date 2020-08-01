0c9be12494f0bfbaddba60e9be457e3c2109984d
EkaterinaLisovec <lisovets_ekaterina@mail.ru>
2020-08-01 16:51:27 +0300

    upd class

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,13 +1,13 @@
 public class Mobile {
     public static void main(String[] args) {
-        float current_account = 1000.01F; //INPUT
-        float transfer = 2000.01F; //INPUT
+        float currentAccount = 1000.01F; //INPUT
+        float transfer = 1000.01F; //INPUT
         int bonus = 0;
         if (transfer > 1000) {
             bonus = (int) transfer / 100;
         }
-        float total_account = current_account + transfer + bonus;
+        float totalAccount = currentAccount + transfer + bonus;
         System.out.println("Bonus = " + bonus);
-        System.out.println("Total account = " + total_account);
+        System.out.println("Total account = " + totalAccount);
     }
 }
